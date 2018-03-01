b5c807d7312f8a6ae23d4849aac1b74968e9501e
anshuladlakha <anshuladlakha@users.noreply.github.com>
2018-03-01 20:27:32 +0530

    undoing wrong commit

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-      $('#contactPgLink').click(function(e){
-            e.preventDefault();
-            $('html, body').animate({
-                scrollTop: $("#fourthPage").offset().top
-            }, 1000);
-      });
-});
